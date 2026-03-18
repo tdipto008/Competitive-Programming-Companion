49f431fdc1d675035a6fbf0df1fabb6941711bfa
Devjyoti Tikader Dipto <tdipto008@gmail.com>
2026-03-18 23:56:34 +0600

    Create template.cpp

---
+++ CPC_Files/template.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include "C:/CPC_Files/debug.h"
+#else
+#define dbg(...)
+#endif
+
+#define endl "\n"
+#define ll long long
+
+void solve() {
+    
+}
+
+signed main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int TT = 1;
+    cin >> TT;
+
+    while (TT--) {
+        solve();
+    }
+    return 0;
+}
